1d12822563873fd9483a5f6f1353a549f237eee4
Zexin <zxuu0075@student.monash.edu>
2025-06-12 20:12:11 +1000

    first push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
+// 整个项目的入口， 从这里开始运行
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
